6584ee786f3bd39654bfa6d7dc28bc2528da6522
Grigory Grishin <106533783+GGCoder098@users.noreply.github.com>
2025-07-01 19:57:01 +0100

    Delete GUI directory

--- GUI/lesson1/cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GUIApp_FILE C:/Game stuff/coding projects/c++/GUI/lesson1/cmake-build-debug/GUIApp.exe)
-set(__QT_DEPLOY_TARGET_GUIApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GUIApp_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
